99d2c1b9d925e3fc87ee9fea020d5f70b2ff17c2
Julie Scherer <juliescherer@CHIIT008M.hsd1.il.comcast.net>
2023-08-10 14:12:18 -0500

    Working Generate DDL DAG and backup of yesterday's version

--- airflow/dags/generate_ddl_dag/archive/Generate_DDL_DAG_20230809.py
+++ airflow/dags/generate_ddl_dag/archive/Generate_DDL_DAG_20230809.py
@@ -7,66 +7,46 @@ from datetime import datetime, timedelta
 import pendulum
 import pandas as pd
 
-# Import decorators and classes from the SDK
-from astro import sql as aql
-from astro.files import File
-from astro.sql.table import Table
-
+from airflow import DAG
 from airflow.decorators import dag, task
 from airflow.models import Variable
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.task_group import TaskGroup
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 
 from include.functions import ms_teams_callback_functions
 
+from airflow.models import Connection
+from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
+from airflow.settings import Session
+
+from astro import sql as aql
+from astro.files import File, get_file_list
+from astro.sql.table import Metadata, Table
+from astro.constants import FileType
+
+from include.functions import ms_teams_callback_functions
+import include.sql.backfill_sql_statements as sql_stmts
+
 ## Global variables 
-
-author = 'Julie Scherer'
-
 SNOWFLAKE_DATABASE = 'ARES'
 SNOWFLAKE_SCHEMA = 'ETL'
-MSSQL_DATABASE = 'CREO'
 
-FILE_FORMAT = """
-    TYPE = CSV
-    FIELD_DELIMITER = '|'
-    RECORD_DELIMITER = '\\n'
-    SKIP_HEADER = 1
-    NULL_IF = 'NULL'
-"""
+DATABASE_NAME = 'CREO'
+TABLE_LIST = ['DatasetValue']
+
+MSSQL_CONN = "awsmssql_creosql_creo_conn"
+MSSQL_HOOK = MsSqlHook(mssql_conn_id=MSSQL_CONN)
+
+SF_CONN = "snowflake_default"
+SF_HOOK = SnowflakeHook(snowflake_conn_id=SF_CONN)
 
 S3_BUCKET_NAME = Variable.get("s3_etldata_bucket_var") #s3-dev-etldata-001
 
 START_DATE = pendulum.datetime(2023, 7, 1, tz='US/Eastern')
-
-SF_CONN_ID = "snowflake_default"
-S3_CONN_ID = "aws_s3_conn"
-
-def get_mssql_hook():
-	"""
-	Returns a MSSQL hook for interacting with MSSQL.
-	"""
-	MSSQL_CONN = "awsmssql_creosql_creo_conn"
-	MSSQL_HOOK = MsSqlHook(mssql_conn_id=MSSQL_CONN)
-	return MSSQL_HOOK
-
-def get_s3_hook():
-	"""
-	Returns a S3 hook for interacting with S3.
-	"""
-	S3_CONN = "aws_s3_conn"
-	S3_HOOK = S3Hook(aws_conn_id=S3_CONN)
-	return S3_HOOK
-
-def get_sf_hook():
-	"""
-	Returns a Snowflake hook for interacting with Snowflake.
-	"""
-	SF_CONN = "snowflake_default"
-	SF_HOOK = SnowflakeHook(snowflake_conn_id=SF_CONN)
-	return SF_HOOK
-
 
 # - - - - - - - - - - - - - - - - - - - -
 
@@ -115,8 +95,8 @@ BEGIN
                 WHEN t.name = 'hierarchyid' THEN 'VARIANT'
                 WHEN t.name = 'geometry' THEN 'VARIANT'
                 WHEN t.name = 'geography' THEN 'VARIANT'
-                WHEN t.name = 'varbinary' THEN 'VARCHAR'
-                WHEN t.name = 'binary' THEN 'VARCHAR'
+                WHEN t.name = 'varbinary' THEN 'VARBINARY'
+                WHEN t.name = 'binary' THEN 'BINARY'
                 WHEN t.name = 'char' THEN CONCAT('CHAR(', t.max_length, ')')
                 WHEN t.name = 'timestamp' THEN 'TIMESTAMP'
                 WHEN t.name = 'sysname' THEN CONCAT('STRING(', t.max_length, ')')
@@ -150,17 +130,42 @@ FROM #TableStats
 ORDER BY TableName;
 """
 
+
+
+def get_mssql_hook():
+	"""
+	Returns a MSSQL hook for interacting with MSSQL.
+	"""
+	MSSQL_CONN = "awsmssql_creosql_creo_conn"
+	MSSQL_HOOK = MsSqlHook(mssql_conn_id=MSSQL_CONN)
+	return MSSQL_HOOK
+
+def get_s3_hook():
+	"""
+	Returns a S3 hook for interacting with S3.
+	"""
+	S3_CONN = "aws_s3_conn"
+	S3_HOOK = S3Hook(aws_conn_id=S3_CONN)
+	return S3_HOOK
+
+def get_sf_hook():
+	"""
+	Returns a Snowflake hook for interacting with Snowflake.
+	"""
+	SF_CONN = "snowflake_default"
+	SF_HOOK = SnowflakeHook(snowflake_conn_id=SF_CONN)
+	return SF_HOOK
+
+
 # - - - - - - - - - - - - - - - - - - - -
 
 # Default DAG args
 default_args = {
-    'owner': author,
     "retries": 0,
     "retry_delay": timedelta(minutes=1),
     'execution_timeout': timedelta(hours=3),
     "on_failure_callback": ms_teams_callback_functions.failure_callback
 }
-
 # Initialize the DAG
 @dag(
     "Generate_DDL_DAG",
@@ -173,63 +178,55 @@ default_args = {
     template_searchpath="/usr/local/airflow/include",
 )
 def Generate_DDL_DAG():
+
     @task
     def create_snowflake_table():
         create_ddl_table = f"""
-            CREATE OR REPLACE TABLE {SNOWFLAKE_DATABASE}.{SNOWFLAKE_SCHEMA}.{MSSQL_DATABASE}_DDL (
-                Table_Name VARCHAR(128),
-                Column_Data VARCHAR
+            CREATE TABLE IF NOT EXISTS {SNOWFLAKE_SCHEMA}.{SNOWFLAKE_DATABASE}_DDL (
+                TableName VARCHAR(128),
+                ColumnData VARCHAR
             );
         """
         get_sf_hook().run(create_ddl_table)
 
     @task
     def mssql_to_s3():
-        df = get_mssql_hook().get_pandas_df(get_table_definitions)
-        logging.info(f"df = {df}")
-
-        out_dir=f"include/DDL/{SNOWFLAKE_DATABASE}/{SNOWFLAKE_SCHEMA}"
-        os.makedirs(out_dir, exist_ok=True)
-        csv_file=f"{MSSQL_DATABASE}_DDL.csv"
-        file_path = os.path.join(out_dir, csv_file)
-
-        df.to_csv(
-            file_path,
-            header=True,  # Data file needs to have a header row
+        df = MSSQL_HOOK.get_pandas_df(get_table_definitions)
+        df_byte = df.to_csv(
+            header=False,  # Exclude the column headers from the CSV
             index=False,  # Exclude the row index from the CSV
             sep="|",  # Use the pipe symbol as the column separator
             na_rep='NULL',  # Replace missing values with 'NULL'
+            compression='gzip',  # Compress the CSV file using gzip
             doublequote=True,  # Enable double quoting for values
-        )
+            quotechar='"'
+        ).encode()
 
         # Load zip file to S3
-        # https://airflow.apache.org/docs/apache-airflow/1.10.4/_api/airflow/hooks/S3_hook/index.html#airflow.hooks.S3_hook.S3Hook.load_file
-        get_s3_hook().load_file(
-            filename=file_path,
-            key=f"inbound/{MSSQL_DATABASE}/DDL/{MSSQL_DATABASE}_DDL.csv",
+        get_s3_hook().load_bytes(
+            bytes_data=df_byte,
             bucket_name=S3_BUCKET_NAME,
             replace=True,
+            key=f"inbound/{DATABASE_NAME}/DDL/{DATABASE_NAME}_DDL.csv.gz",
         )
-
-        os.remove(file_path)
-        os.removedirs(out_dir)
 
     @task
     def s3_to_snowflake():
-        get_sf_hook().run(f"TRUNCATE TABLE IF EXISTS {SNOWFLAKE_DATABASE}.{SNOWFLAKE_SCHEMA}.{MSSQL_DATABASE}_DDL")
-        sql_query = f"""
-			COPY INTO {SNOWFLAKE_DATABASE}.{SNOWFLAKE_SCHEMA}.{MSSQL_DATABASE}_DDL
-			FROM (
-				SELECT $1, $2 
-				FROM @ETL.INBOUND/{MSSQL_DATABASE}/DDL/
-			)
-			FILE_FORMAT = ( 
-				{FILE_FORMAT}
-			) 
-			PATTERN = '.*{MSSQL_DATABASE}_DDL.csv'    
-		"""
-        get_sf_hook().run(sql_query)
-	
+        s3_to_snowflake = aql.load_file(
+            task_id="s3_to_snowflake",
+            input_file=File(
+                path=f"s3://{S3_BUCKET_NAME}/inbound/inbound/{DATABASE_NAME}/DDL/{DATABASE_NAME}_DDL.csv.gz", 
+                filetype=FileType.CSV
+            ),
+            output_table=Table(
+                conn_id="snowflake_default",
+                metadata=Metadata(database=SNOWFLAKE_DATABASE, schema=SNOWFLAKE_SCHEMA),
+                name=f"{SNOWFLAKE_SCHEMA}.{SNOWFLAKE_DATABASE}_DDL",
+            ),
+            if_exists="replace",
+        )
+        aql.cleanup() 
+
     create_snowflake_table_task = create_snowflake_table()
     mssql_to_s3_task = mssql_to_s3()
     s3_to_snowflake_task = s3_to_snowflake()
